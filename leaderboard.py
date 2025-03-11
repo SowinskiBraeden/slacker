6c1ca2c78340c948efd783c2643f46df5abbb82d
SowinskiBraeden <braeden@sowinski.dev>
2025-03-10 23:24:48 -0700

    initial commit

---
+++ leaderboard.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python3
+from typing import List, Dict
+import json
+from prettytable import PrettyTable
+import os, shutil
+
+def pretty_table(dct: Dict, title: str, add: str=""):
+  table = PrettyTable()
+  for c in dct.keys():
+     table.add_column(c, [])
+  table.add_row(['\n'.join(dct[c]) for c in dct.keys()])
+  table.align = "l"
+  with open(f"./tables/{title}.txt", "a") as file:
+    if add != "":
+      file.write(f"{add}\n")
+    file.write(table.__str__())
+    file.write("\n\n")
+  # print(table)
+
+def setupDir() -> None:
+  if not os.path.exists("./tables"): 
+    os.makedirs("./tables") 
+
+  folder = './tables'
+  for filename in os.listdir(folder):
+    file_path = os.path.join(folder, filename)
+    try:
+      if os.path.isfile(file_path) or os.path.islink(file_path):
+        os.unlink(file_path)
+      elif os.path.isdir(file_path):
+        shutil.rmtree(file_path)
+    except Exception as e:
+      print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+def main() -> None:
+  teams: List[Dict[str, str|List]] = {}
+  with open("teams.json", "r") as file:
+    teams = json.load(file)
+
+  projects_per_campus = {
+    "Burnaby": [],
+    "Downtown": []
+  }
+  projects_per_set = {
+    "A": [],
+    "B": [],
+    "C": [],
+    "D": [],
+    "E": [],
+    "F": []
+  }
+  projects_all_time = []
+  contributors_per_campus = {
+    "Burnaby": [],
+    "Downtown": []
+  }
+  contributors_per_set = {
+    "A": [],
+    "B": [],
+    "C": [],
+    "D": [],
+    "E": [],
+    "F": []
+  }
+  contributors_per_project = {}
+  contributors_all_time = []
+  for team in teams:
+    added, deleted, commits = 0, 0, 0
+    for author in team["contributors"]:
+      added += author["added"]
+      deleted += author["deleted"]
+      commits += author["commits"]
+
+      author["contributed"] = author["added"] - author["deleted"]
+      
+      contributors_per_campus[team["campus"]].append(author)
+      contributors_per_set[team["set"]].append(author)
+
+      if team["id"] not in contributors_per_project:
+        contributors_per_project[team["id"]] = []
+      contributors_per_project[team["id"]].append(author)
+
+      contributors_all_time.append(author)
+
+    size = added - deleted
+
+    project = {
+      "team": team["id"],
+      "size": size,
+      "added": added,
+      "deleted": deleted,
+      "commits": commits
+    }
+
+    projects_per_campus[team["campus"]].append(project)
+    projects_per_set[team["set"]].append(project)
+    projects_all_time.append(project)
+
+  #######################################################################
+
+  # Largest project per campus
+  for campus in projects_per_campus:        
+    filtered = sorted(projects_per_campus[campus], key=lambda d: d['size'], reverse=True)
+    
+    data = {
+      "Rank": [],
+      "Team": [],
+      "Size": []
+    }
+
+    rank = 0
+    for project in filtered:
+      rank += 1
+      data["Rank"].append(str(rank))
+      data["Team"].append(project["team"])
+      data["Size"].append(str(project["size"]))
+
+    pretty_table(data, "LargestProjectsPerCampus", f"{campus} Campus")
+
+  # Largest project per set
+  for set in projects_per_set:
+    filtered = sorted(projects_per_set[set], key=lambda d: d['size'], reverse=True)
+    
+    data = {
+      "Rank": [],
+      "Team": [],
+      "Size": []
+    }
+
+    rank = 0
+    for project in filtered:
+      rank += 1
+      data["Rank"].append(str(rank))
+      data["Team"].append(project["team"])
+      data["Size"].append(str(project["size"]))
+
+    pretty_table(data, "LargestProjectPerSet", f"Set {set}")
+
+  # Largest project of all
+  filtered = sorted(projects_all_time, key=lambda d: d['size'], reverse=True)
+  
+  data = {
+    "Rank": [],
+    "Team": [],
+    "Size": []
+  }
+
+  rank = 0
+  for project in filtered:
+    rank += 1
+    data["Rank"].append(str(rank))
+    data["Team"].append(project["team"])
+    data["Size"].append(str(project["size"]))
+
+  pretty_table(data, "LargestProjectAllTeams")
+
+
+  # Contributors ranker per campus
+  for campus in contributors_per_campus:
+    filtered = sorted(contributors_per_campus[campus], key=lambda d: d['added'], reverse=True)
+    
+    data = {
+      "Rank": [],
+      "Author": [],
+      "Commits": [],
+      "Added": [],
+      "Deleted": [],
+      "Actual": [],
+    }
+
+    rank = 0
+    for author in filtered:
+      rank += 1
+      data["Rank"].append(str(rank))
+      data["Author"].append(author['author'])
+      data["Commits"].append(str(author['commits']))
+      data["Added"].append(f"+{author['added']}")
+      data["Deleted"].append(f"-{author['deleted']}")
+      data["Actual"].append(str(author['contributed']))
+
+    pretty_table(data, "TopContributorPerCampus", f"{campus} Campus")
+
+
+  # Contributors ranked per set
+  for set in contributors_per_set:
+    filtered = sorted(contributors_per_set[set], key=lambda d: d['added'], reverse=True)
+    
+    data = {
+      "Rank": [],
+      "Author": [],
+      "Commits": [],
+      "Added": [],
+      "Deleted": [],
+      "Actual": [],
+    }
+
+    rank = 0
+    for author in filtered:
+      rank += 1
+      data["Rank"].append(str(rank))
+      data["Author"].append(author['author'])
+      data["Commits"].append(str(author['commits']))
+      data["Added"].append(f"+{author['added']}")
+      data["Deleted"].append(f"-{author['deleted']}")
+      data["Actual"].append(str(author['contributed']))
+
+    pretty_table(data, "TopContributorPerSet", f"Set {set}")
+
+
+  # Contributors ranked per Team
+  for project in contributors_per_project:
+    filtered = sorted(contributors_per_project[project], key=lambda d: d['added'], reverse=True)
+
+    data = {
+      "Rank": [],
+      "Author": [],
+      "Commits": [],
+      "Added": [],
+      "Deleted": [],
+      "Actual": [],
+    }
+
+    rank = 0
+    for author in filtered:
+      rank += 1
+      data["Rank"].append(str(rank))
+      data["Author"].append(author['author'])
+      data["Commits"].append(str(author['commits']))
+      data["Added"].append(f"+{author['added']}")
+      data["Deleted"].append(f"-{author['deleted']}")
+      data["Actual"].append(str(author['contributed']))
+
+    pretty_table(data, "TopContributorsPerTeam", project)
+
+  # Contributors ranked all time
+  contributors_all_time = sorted(contributors_all_time, key=lambda d: d['added'], reverse=True)
+  
+  data = {
+    "Rank": [],
+    "Author": [],
+    "Actual": [],
+    "Added": [],
+    "Deleted": [],
+  }
+  
+  rank = 0
+  for author in contributors_all_time:
+    rank += 1
+    data["Rank"].append(str(rank))
+    data["Author"].append(author['author'])
+    data["Actual"].append(str(author["contributed"]))
+    data["Added"].append(f"+{author['added']}")
+    data["Deleted"].append(f"-{author['deleted']}")
+
+  pretty_table(data, "TopContributorsAllTime")
+
+if __name__ == "__main__":
+  setupDir()
+  main()
---
+++ main.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+from links import urls
+from typing import List, Dict
+import json
+import asyncio
+import aiohttp
+import time
+from tqdm import tqdm
+
+from parse import parse
+from key import token
+
+MAX_RETRY: int = 5
+
+async def getInsights(repoURL: str, attempt: int = 0) -> Dict[str, str|None] | None:
+  owner: str = repoURL.split("/")[3]
+  repo: str = repoURL.split("/")[4]
+
+  # print(f"Querying {repo}")
+
+  query: str = f"https://api.github.com/repos/{owner}/{repo}/stats/contributors"
+  headers: Dict[str, str] = {
+    "Accept": "application/vnd.github+json",
+    "Authorization": f"Bearer {token}",
+    "X-GitHub-Api-Version": "2022-11-28" 
+  }
+
+  async with aiohttp.ClientSession() as session:
+    async with session.get(query, headers=headers, timeout=5) as resp:
+      # print(f"   {urls.index(repoURL) + 1}/{len(urls)} - {resp.status}")
+      if resp.status > 400:
+        # print(f"An Error Occured Querying {repo}")
+        return None
+      if resp.status == 202 and attempt <= MAX_RETRY:
+        # print(f"{repo} Was queried, but no data was received")
+        time.sleep(10)
+        attempt += 1
+        team = await getInsights(repoURL, attempt)
+        return team
+      elif attempt > MAX_RETRY: return None
+
+      team: Dict[str, str|List] = {
+        "repo": repoURL,
+        "team": repo,
+        "contributors": []
+      }
+      
+      data: List[Dict[str, any]] = await resp.json()
+
+      for contributor in data:
+        newContributor: Dict[str, str|int] = {
+          "author": contributor["author"]["login"],
+          "commits": contributor["total"],
+          "added": 0,
+          "deleted": 0
+        }
+
+        for week in contributor["weeks"]:
+          newContributor["added"] += week["a"]
+          newContributor["deleted"] += week["d"]
+
+        team["contributors"].append(newContributor)
+
+  return team
+
+async def main() -> None:
+  teamsOriginal: List[Dict] = []
+  with open("teams.json", "r") as file:
+    teamsOriginal = json.load(file)
+
+  teams: List[Dict] = []
+
+  for i in tqdm(range(len(urls)),
+                desc="Reading Insights",
+                ascii=False, ncols=75):
+    url = urls[i]
+    team: Dict[str, str|List] = await getInsights(url)
+    if team is not None:
+      teams.append(team)
+    time.sleep(5)
+
+  print(f"Completed {len(teams)}/{len(urls)}")
+
+  # Parse gets Campus, Set, and Team ID
+  teams = parse(teams)
+
+  for originalTeam in teamsOriginal:
+    originalTeamID = originalTeam["id"]
+    exists: bool = False
+    for team in teams:
+      if team["id"] == originalTeamID:
+        exists = True
+        break
+
+    if not exists:
+      teams.append(originalTeam)
+
+  with open("teams.json", "w") as file:
+    json.dump(teams, file, indent=2)
+
+if __name__ == "__main__":
+  asyncio.run(main())
